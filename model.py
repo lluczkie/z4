fixed data split, move prediction from data sample, precision score

model.py
@@ -3,6 +3,7 @@ import numpy as np
 from snake import Direction
 from math import log
 import json
+from  sklearn.metrics import precision_score
 """Implement your model, training code and other utilities here. Please note, you can generate multiple 
 pickled data files and merge them into a single data list."""
 
@@ -146,25 +147,27 @@ def ID3(training_data, directions):
     return {str(divisor): {True: ID3(attr_true, dir_true), False: ID3(attr_false, dir_false)}}
 
 def split_data(data, dirs):
-    attribute_ixs = data[0:1, :]
-    data=data[1:, :]
-    permutation = np.random.permutation(data.shape[0])
-    np.take(data, permutation, axis=0, out=data)
+    permutation = np.random.permutation(data.shape[0]-1)
+    np.take(data[1:, :], permutation, axis=0, out=data[1:, :])
     np.take(dirs, permutation, out=dirs)
     split_ix = data.shape[0] // 5
-    test_data = data[:split_ix, :]
-    train_data = data[split_ix:, :]
-    test_dirs = dirs[:split_ix]
-    train_dirs = dirs[split_ix:]
-
-    train_data = np.concatenate((attribute_ixs, train_data), axis=0)
-    test_data = np.concatenate((attribute_ixs, test_data), axis=0)
+    train_data = data[:4*split_ix+1, :]
+    test_data = data[4*split_ix+1:, :]
+    train_dirs = dirs[:4*split_ix]
+    test_dirs = dirs[4*split_ix:]
 
     return  (train_data, train_dirs), (test_data, test_dirs)
 
+def act_from_data_sample(id3, data_sample):
+    level = list(id3.keys())[0]
+    next = id3[level][str(bool(data_sample[int(level)])).lower()]
+    while type(next) == dict:
+        level = list(next.keys())[0]
+        next=next[level][str(bool(data_sample[int(level)])).lower()]
+    return int(next)
+
 if __name__ == "__main__":
     combine_pickles()
-    run=0
     states, directions = get_states_and_directions_from_pickle(f"data/merged.pickle")
     processed_data, processed_directions = process_data(states, directions, 30, (300, 300))
     (train_data, train_dirs), (test_data, test_dirs) = split_data(processed_data, processed_directions)
@@ -172,3 +175,13 @@ if __name__ == "__main__":
     out_file = open(f"tree.json", "w")
     json.dump(tree, out_file, indent = 2)
     out_file.close()
+
+    f = open('tree.json', 'r')
+    id3 = json.load(f)
+    f.close()
+
+    pred_dirs = []
+    for sample in test_data:
+        pred_dirs.append(act_from_data_sample(id3, sample))
+    ps = precision_score(test_dirs, pred_dirs, average=None)
+    pass