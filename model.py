1b1f81cc39f6442bfa524115c91f6c3facf8b086
Lidia <lidia.luczkiewicz@pw.edu.pl>
2024-12-03 20:18:58 +0100

    commited main instead of model

--- model.py
+++ model.py
@@ -179,7 +179,7 @@ if __name__ == "__main__":
     processed_data, processed_directions = process_data(states, directions, 30, (300, 300))
     
     accuracies = []
-    percents = [1, 5, 10, 15, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100]
+    percents = [100]
     for percent in percents:
         (train_data, train_dirs), (test_data, test_dirs) = split_data(processed_data, processed_directions, percent)
         tree = ID3(train_data, train_dirs)
